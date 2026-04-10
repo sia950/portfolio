ef216c5cc497850402815e6ad206c72507088418
sia950 <kim950136@gmail.com>
2026-04-10 11:29:29 +0300

    fix deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 export default defineConfig({
-  base: '/portfolio/',
+  base: '/portfolio_1/',
 
   plugins: [
     react({
